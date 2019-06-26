fcbe4d33420bc582d8cc703f7e803296126c959f
lucasaires <lucas.aires@ccc.ufcg.edu.br>
2019-06-26 00:52:35 -0300

    doente de amoooor procurei remedio na vida noturna

--- logico/main.pl
+++ logico/main.pl
@@ -1,33 +1,31 @@
 :- initialization main.
 
-main(X):-
-    repeat,
-    write(".::. PrompSkoob .::." ),
-    write(" - Menu Principal - "), 
-    write(" Ola, "),
-    write(" (1) Editar meu perfil"),
-    write(" (2) Gerenciar livro"),
-    write(" (3) Minha estante"),
-    write(" (4) Pesquisar no acervo"),
-    write(" (5) Recomendações de livros"),
-    write(" (6) Remover meu perfil"),
-    write(" (7) Sair da conta"),
-    write(" (8) Sair"),
-    write("Opcao: "),
+main:-
+    write(".::. PrompSkoob .::.
+    - Menu Principal -  
+    Ola
+    (1) Editar meu perfil
+    (2) Gerenciar livro
+    (3) Minha estante
+    (4) Pesquisar no acervo
+    (5) Recomendações de livros
+    (6) Remover meu perfil
+    (7) Sair da conta
+    (8) Sair
+    Opcao: "),
     read(X),
-    (X == 1 -> editarPerfil();
+    (X == 1 -> editarPerfil(X);
      X == 2 -> gerenciarLivro();
      X == 3 -> minhaEstante();
      X == 4 -> pesquisarAcervo();
      X == 5 -> recomendaçõesLivros();
      X == 6 -> removerPerfil();
      X == 7 -> sairConta();
-     X == 8 -> sair()),
-    halt(0).
+     X == 8 -> sair()).
 
 
-editarPerfil():-
-    write("Até Breve! ").
+editarPerfil(X):- 
+    (X == 1 -> write("Até Breve! ")).
 
 gerenciarLivro():-
     write("Até Breve! ").
