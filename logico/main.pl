97f9512ce0f1607f8557baeab7be0a071095eca5
Caio Simplício <caio.simplicio@ccc.ufcg.edu.br>
2019-06-27 18:16:58 -0300

    adicionado o menu criado por lucas

---
+++ logico/main.pl
@@ -0,0 +1,51 @@
+
+editarPerfil(X):- 
+    (X == 1 -> write("Até Breve! ")).
+
+gerenciarLivro():-
+    write("Até Breve! ").
+
+minhaEstante():-
+    write("Até Breve! ").
+    
+
+pesquisarAcervo:-
+    write("Até Breve! ").
+    
+
+recomendaçõesLivros:-
+    write("Até Breve! ").
+
+removerPerfil:-
+    write("Até Breve! ").
+
+sairConta:-
+    write("Até Breve! ").
+
+
+sair:-
+    write("Até Breve! ").
+
+:- initialization main.
+main:-
+    write(".::. PrompSkoob .::.
+    - Menu Principal -  
+    Ola
+    (1) Editar meu perfil
+    (2) Gerenciar livro
+    (3) Minha estante
+    (4) Pesquisar no acervo
+    (5) Recomendações de livros
+    (6) Remover meu perfil
+    (7) Sair da conta
+    (8) Sair
+    Opcao: "),
+    read(X),
+    (X == 1 -> editarPerfil(X);
+     X == 2 -> gerenciarLivro();
+     X == 3 -> minhaEstante();
+     X == 4 -> pesquisarAcervo();
+     X == 5 -> recomendaçõesLivros();
+     X == 6 -> removerPerfil();
+     X == 7 -> sairConta();
+     X == 8 -> sair()).
